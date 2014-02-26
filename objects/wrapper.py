7ee14592873395aa3a664d1475a7e7f1fef724f9
Tim Keefer <tkeefer@gmail.com>
2014-02-25 16:50:27 -0800

    move dirs

---
+++ objects/wrapper.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+class Wrapper(object):
+
+	def __init__(self, obj):
+		self.obj = obj
+		self.foo = 'boo'
+
+	def __getattr__(self, attr):
+		wattr = getattr(self.obj, attr)
+		return wattr
+
+if __name__ == '__main__':
+	from myobject import Animal
+	
+	riley = Animal()
+	wrapper = Wrapper(riley)
+
+	print(wrapper.noise)
+	print(wrapper.has_owner)
+	print(wrapper.foo)
+	print(wrapper.boo)
